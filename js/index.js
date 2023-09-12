740b549f91160120edfb684ca444aa38d42f9943
Shekhar Shrestha <shekharr.shresthaa@gmail.com>
2023-09-12 20:08:31 +0545

    fix: redesigned website

--- js/index.js
+++ js/index.js
@@ -625,6 +625,38 @@ $('.services-row').slick({
   ]
 });
 
+
+
+$('.testimonial-slider').slick({
+  dots: true,
+  infinite: true,
+  loop: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-chevron-left left-arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right right-arrow"></i>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
 window.addEventListener('load', function () {
   if (window.innerWidth > 992) {
     const sectionPairs = document.querySelectorAll('.about-us-flex');
@@ -641,3 +673,49 @@ window.addEventListener('load', function () {
 
 
 
+$(document).ready(function () {
+  $('.dropdown').hover(function () {
+    $(this).addClass('show');
+    $(this).find('.dropdown-menu').addClass('show');
+  }, function () {
+    $(this).removeClass('show');
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var paragraphs = document.querySelectorAll(".testimonial-slider .data");
+  paragraphs.forEach(function (paragraph) {
+    var text = paragraph.textContent;
+    if (text.length > 320) {
+      var truncatedText = text.slice(0, 320);
+      paragraph.textContent = (truncatedText + "..");
+      var readMoreLink = document.createElement("a");
+      readMoreLink.href = "/reviews.html";
+      readMoreLink.textContent = " Read More";
+      readMoreLink.classList.add("fw-600")
+      paragraph.appendChild(readMoreLink);
+    }
+  });
+
+  function setEqualHeight(columns) {
+      var tallestColumn = 0;
+      columns.forEach(function(column) {
+          column.style.height = "auto";
+          var columnHeight = column.offsetHeight;
+          if (columnHeight > tallestColumn) {
+              tallestColumn = columnHeight;
+          }
+      });
+      columns.forEach(function(column) {
+          column.style.height = tallestColumn + "px";
+      });
+  }
+
+  var columns = document.querySelectorAll(".testimonial-slider .column");
+  setEqualHeight(columns);
+
+  window.addEventListener("resize", function() {
+      setEqualHeight(columns);
+  });
+});
